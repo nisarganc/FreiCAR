063d595aaf14cc767078d3a878732b3fa056b321
Nisarga Nilavadi <nisarganc7@gmail.com>
2024-03-11 16:18:53 +0100

    Delete convertor.py

--- convertor.py
+++
@@ -1,5 +0,0 @@
-from moviepy.editor import VideoFileClip
-
-videoClip = VideoFileClip("3_localization/localization_lowvariance.mp4")
-
-videoClip.write_gif("3_localization/localization_lowvariance.gif")
